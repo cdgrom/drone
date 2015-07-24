2d6441c28c34c78b64d7f0a691e4da988cb4197d
V3506 (Conference ID) <v3506@cam.ac.uk>
2015-07-24 10:10:37 +0100

    1010, many errors

--- drone-backend.js
+++ drone-backend.js
@@ -12,11 +12,52 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+
+    .device("nav", {
+        driver: "ardrone-nav",      // Combine with a second device to have more information
+        connection: "ardrone"
+    })
     .on("ready", fly);
-    
+
 // Fly the bot
 function fly(robot) {
 
+    // Only retrieve a limited amount of navigation data
+    // As recommended by Parrot AR Drone developer's guide
+    bot.drone.config('general:navdata_demo', 'TRUE');
+
+    bot.nav.on("navdata", function(data) {
+        console.log(data);
+    });
+
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+
+    while (bot.drone.altitude < 200) {
+        bot.drone.up(1);
+    }
+
+    for (var i = 0; i < 1; i = i + 0.1) {
+        bot.drone.front(i);
+    }
+
+    bot.drone.land();
+
+    after(10*1000, function() {
+        bot.drone.takeoff();
+    });
+
+    while (bot.drone.altitude < 200) {
+        bot.drone.up(1);
+    }
+
+    for (var i = 0; i < 1; i = i+0.1) {
+        bot.drone.back(i);
+    }
 }
 
 Cylon.start();
+
+fly(drone);
